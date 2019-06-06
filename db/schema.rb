a02a5d14641e25c6c0704cc252ee1acd9a26a103
gmontano <gilberto.montano@crol.mx>
2019-06-06 14:56:15 -0700

    add customer type

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190605210644) do
+ActiveRecord::Schema.define(version: 20190606194048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customer_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
---
+++ test/system/customer_types_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CustomerTypesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit customer_types_url
+  #
+  #   assert_selector "h1", text: "CustomerType"
+  # end
+end
